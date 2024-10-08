4a97468902a26fa6b77b6f48f8aa06fd8181d4db
Khoa <kknguyen8@myseneca.ca>
2024-10-07 22:41:10 -0400

    p1.s4 completed

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const HTTP_PORT = process.env.PORT || 8080;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.redirect("/about");
+});
+
+app.get("/about", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/about.html"));
+});
+
+// Start the server
+app.listen(HTTP_PORT, () => {
+    console.log(`Express http server listening on ${HTTP_PORT}`);
+});
