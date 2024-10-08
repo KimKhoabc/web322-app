37b9ef8885534af19c2b592d6ae02db18923e922
Khoa <kknguyen8@myseneca.ca>
2024-10-08 00:07:51 -0400

    credit

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
+/*********************************************************************************
+
+WEB322 – Assignment 02
+I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source (including 3rd party web sites) or distributed to other students.
+
+Name: Kim Khoa Nguyen
+Student ID: 104574231
+Date: October 08, 2024
+Replit Web App URL: https://replit.com/@kimkhoa0905/web322-app
+GitHub Repository URL: https://github.com/KimKhoabc/web322-app
+
+********************************************************************************/
+
 const storeService = require('./store-service');
 const express = require('express');
 const path = require('path');
